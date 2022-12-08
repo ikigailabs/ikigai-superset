97d9ee0e2c6b723c0a440855cd7d9f1a241f0d17
Anmol Majithia <majithiaanmol12@gmail.com>
2022-12-08 17:02:21 -0500

    [UIUX-861][feature][3] compile fix 2

--- superset-frontend/src/explore/controls.jsx
+++ superset-frontend/src/explore/controls.jsx
@@ -330,7 +330,7 @@ export const controls = {
         'The options here are defined on a per database ' +
         'engine basis in the Superset source code.',
     ),
-    visibility: false,
+    visibility: () => Boolean(false),
     mapStateToProps: state => ({
       choices: state.datasource ? state.datasource.time_grain_sqla : null,
     }),
