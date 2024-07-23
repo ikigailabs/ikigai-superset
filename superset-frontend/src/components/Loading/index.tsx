b267e4ffbc2d0d3330951eddedfb1932a24bdafe
Kevin Aliberti <kevin@ikigailabs.io>
2024-07-23 12:23:45 -0400

    merged branch ikigailabs-dev into ikigailabs-dev-3.0

--- superset-frontend/src/components/Loading/index.tsx
+++ superset-frontend/src/components/Loading/index.tsx
@@ -78,7 +78,30 @@ export default function Loading({
       aria-live="polite"
       aria-label="Loading"
     >
-      <img src={image || Loader} alt="Loading..." />
+      <svg
+        id="Identity"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 570 570"
+      >
+        <defs>
+          <style>{`.cls-1{fill:url(#linear-gradient);}`}</style>
+          <linearGradient
+            id="linear-gradient"
+            x1="102.8"
+            y1="411.52"
+            x2="427.29"
+            y2="87.02"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".17" stopColor="#aa83ff" />
+            <stop offset=".75" stopColor="#7822ff" />
+          </linearGradient>
+        </defs>
+        <path
+          className="cls-1"
+          d="m372.93,326.81l-86.68,86.68-86.68-86.68,86.68-86.68,86.68,86.68Zm-272.09,98.68h173.36l-86.68-86.68-86.68,86.68Zm284.08-110.73v-86.68s0-86.68,0-86.68l-86.68,86.68,86.68,86.68Zm-110.76-86.68l-86.68-86.68v86.68s0,86.68,0,86.68l86.68-86.68Zm127.89-22.98l31.85-31.85,31.85-31.85h-63.7s0,63.7,0,63.7Zm-292.75,31.48l30.65,30.65,30.65,30.65v-61.29s-61.29,0-61.29,0Z"
+        />
+      </svg>
     </LoaderImg>
   );
 }
