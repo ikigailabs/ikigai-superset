74a57659367745898474b9890aba1ebed9720ec8
Kevin Aliberti <kevin@ikigailabs.io>
2024-10-02 09:52:12 -0400

    debugging csp

--- superset/config.py
+++ superset/config.py
@@ -1439,32 +1439,9 @@ TALISMAN_ENABLED = utils.cast_to_boolean(os.environ.get("TALISMAN_ENABLED", True
 # }
 
 TALISMAN_CONFIG = {
-    "content_security_policy": {
-        "base-uri": ["'self'"],
-        "default-src": ["'self'"],
-        "img-src": [
-            "'self'",
-            "blob:",
-            "data:",
-            "https://apachesuperset.gateway.scarf.sh",
-            "https://static.scarf.sh/",
-        ],
-        "worker-src": ["'self'", "blob:"],
-        "connect-src": [
-            "'self'",
-            "https://api.mapbox.com",
-            "https://events.mapbox.com",
-        ],
-        "object-src": "'none'",
-        "style-src": [
-            "'self'",
-            "'unsafe-inline'",
-        ],
-        "script-src": ["'self'", "'strict-dynamic'"],
-    },
-    "content_security_policy_nonce_in": ["script-src"],
-    "force_https": False,
-    "session_cookie_secure": False,
+    "content_security_policy": None,
+    "force_https": True,
+    "force_https_permanent": False,
 }
 
 # React requires `eval` to work correctly in dev mode
