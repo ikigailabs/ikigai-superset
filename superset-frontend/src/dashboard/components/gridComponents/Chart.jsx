10efe726bc398b1d3fcdf01c8c178aaddd2b859a
Kevin Aliberti <kevin@ikigailabs.io>
2025-01-13 12:16:18 -0500

    re-enable missing chart error handling

--- superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
+++ superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
@@ -408,11 +408,9 @@ class Chart extends React.Component {
     const { width } = this.state;
     // this prevents throwing in the case that a gridComponent
     // references a chart that is not associated with the dashboard
-
-    console.info('chart', chart, slice);
-    // if (!chart || !slice) {
-    //   return <MissingChart height={this.getChartHeight()} />;
-    // }
+    if (!chart || !slice) {
+      return <MissingChart height={this.getChartHeight()} />;
+    }
 
     const { queriesResponse, chartUpdateEndTime, chartStatus } = chart;
     const isLoading = chartStatus === 'loading';
