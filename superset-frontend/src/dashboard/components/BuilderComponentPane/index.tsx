414a9d7613232db5cd98af67d8983f5446379740
Kevin Aliberti <kevin@ikigailabs.io>
2025-01-14 10:06:08 -0500

    eslint

--- superset-frontend/src/dashboard/components/BuilderComponentPane/index.tsx
+++ superset-frontend/src/dashboard/components/BuilderComponentPane/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,18 +19,30 @@
  */
 /* eslint-env browser */
 import React from 'react';
-import { rgba } from 'emotion-rgba';
 import Tabs from 'src/components/Tabs';
 import { t, css, SupersetTheme } from '@superset-ui/core';
 import SliceAdder from 'src/dashboard/containers/SliceAdder';
 import dashboardComponents from 'src/visualizations/presets/dashboardComponents';
-import NewColumn from '../gridComponents/new/NewColumn';
-import NewDivider from '../gridComponents/new/NewDivider';
-import NewHeader from '../gridComponents/new/NewHeader';
-import NewRow from '../gridComponents/new/NewRow';
-import NewTabs from '../gridComponents/new/NewTabs';
-import NewMarkdown from '../gridComponents/new/NewMarkdown';
+import NewColumn from '../gridComponents/new/layout/NewColumn';
+import NewDivider from '../gridComponents/new/layout/NewDivider';
+import NewHeader from '../gridComponents/new/layout/NewHeader';
+import NewRow from '../gridComponents/new/layout/NewRow';
+import NewTabs from '../gridComponents/new/layout/NewTabs';
+import NewMarkdown from '../gridComponents/new/layout/NewMarkdown';
 import NewDynamicComponent from '../gridComponents/new/NewDynamicComponent';
+import NewIkiTable from '../gridComponents/new/components/NewIkiTable';
+import NewIkiProcessBuilder from '../gridComponents/new/components/NewIkiProcessBuilder';
+import NewIkiRunPipeline from '../gridComponents/new/components/NewIkiRunPipeline';
+import NewDeepCast from '../gridComponents/new/components/NewDeepCast';
+import NewIkiEitlRow from '../gridComponents/new/components/NewIkiEitlRow';
+import NewIkiEitlColumn from '../gridComponents/new/components/NewIkiEitlColumn';
+import NewDyanmicMarkdown from '../gridComponents/new/components/NewDynamicMarkdown';
+// import NewIkiExplainability from '../gridComponents/new/NewIkiExplainability';
+import NewIkiModelMetrics from '../gridComponents/new/NewIkiModelMetrics';
+import NewIkiDatasetDownload from '../gridComponents/new/components/NewIkiDatasetDownload';
+import NewExternalDatasets from '../gridComponents/new/components/NewExternalDatasets';
+import NewForecast from '../gridComponents/new/components/NewForecast';
+import NewForecastModule from '../gridComponents/new/components/NewForecastModule';
 
 const BUILDER_PANE_WIDTH = 374;
 
@@ -43,15 +56,18 @@ const BuilderComponentPane = ({ topOffset = 0 }) => (
       height: calc(100vh - ${topOffset}px);
       width: ${BUILDER_PANE_WIDTH}px;
     `}
+    className="dashboard-builder-sidepane"
   >
     <div
       css={(theme: SupersetTheme) => css`
         position: absolute;
         height: 100%;
         width: ${BUILDER_PANE_WIDTH}px;
-        box-shadow: -4px 0 4px 0 ${rgba(theme.colors.grayscale.dark2, 0.1)};
-        background-color: ${theme.colors.grayscale.light5};
+        box-shadow: 0 0 0 #fff !important;
+        background-color: #fff;
+        border-left: 1px solid #eee;
       `}
+      className="viewport"
     >
       <Tabs
         data-test="dashboard-builder-component-pane-tabs-navigation"
@@ -60,6 +76,7 @@ const BuilderComponentPane = ({ topOffset = 0 }) => (
           line-height: inherit;
           margin-top: ${theme.gridUnit * 2}px;
           height: 100%;
+          padding: 0 24px;
 
           & .ant-tabs-content-holder {
             height: 100%;
@@ -69,22 +86,29 @@ const BuilderComponentPane = ({ topOffset = 0 }) => (
           }
         `}
       >
-        <Tabs.TabPane
-          key={1}
-          tab={t('Charts')}
-          css={css`
-            height: 100%;
-          `}
-        >
-          <SliceAdder />
-        </Tabs.TabPane>
-        <Tabs.TabPane key={2} tab={t('Layout elements')}>
+        <Tabs.TabPane key={1} tab={t('Layout')}>
           <NewTabs />
           <NewRow />
           <NewColumn />
           <NewHeader />
           <NewMarkdown />
           <NewDivider />
+        </Tabs.TabPane>
+        <Tabs.TabPane key={2} tab={t('Components')}>
+          <NewDyanmicMarkdown />
+          <NewIkiTable />
+          <NewIkiProcessBuilder />
+          <NewIkiRunPipeline />
+          <NewDeepCast />
+          <NewIkiEitlRow />
+          <NewIkiEitlColumn />
+          <NewForecastModule />
+          <NewIkiDatasetDownload />
+          <NewIkiModelMetrics />
+          <NewExternalDatasets />
+          <NewForecast />
+
+          {/* <NewIkiExplainability /> */}
           {dashboardComponents
             .getAll()
             .map(({ key: componentKey, metadata }) => (
@@ -93,6 +117,9 @@ const BuilderComponentPane = ({ topOffset = 0 }) => (
                 componentKey={componentKey}
               />
             ))}
+        </Tabs.TabPane>
+        <Tabs.TabPane key={3} tab={t('Charts')} className="tab-charts">
+          <SliceAdder />
         </Tabs.TabPane>
       </Tabs>
     </div>
--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -41,7 +41,7 @@ import { AntdButton } from 'src/components/';
 import { findPermission } from 'src/utils/findPermission';
 import { Tooltip } from 'src/components/Tooltip';
 import { safeStringify } from 'src/utils/safeStringify';
-import HeaderActionsDropdown from 'src/dashboard/components/Header/HeaderActionsDropdown';
+import ConnectedHeaderActionsDropdown from 'src/dashboard/components/Header/HeaderActionsDropdown';
 import PublishedStatus from 'src/dashboard/components/PublishedStatus';
 import UndoRedoKeyListeners from 'src/dashboard/components/UndoRedoKeyListeners';
 import PropertiesModal from 'src/dashboard/components/PropertiesModal';
@@ -55,7 +55,6 @@ import setPeriodicRunner, {
   stopPeriodicRender,
 } from 'src/dashboard/util/setPeriodicRunner';
 import { PageHeaderWithActions } from 'src/components/PageHeaderWithActions';
-import { Dropdown } from 'src/components/Dropdown';
 import DashboardEmbedModal from '../EmbeddedModal';
 import OverwriteConfirm from '../OverwriteConfirm';
 
@@ -176,7 +175,7 @@ class Header extends React.PureComponent {
   static discardChanges(ikigaiOrigin, supersetUrl) {
     const url = new URL(window.location.href);
     const originalUrl = url.origin + url.pathname;
-    // console.log('Discard - url', url, originalUrl, ikigaiOrigin, supersetUrl);
+    console.log('Discard - url', url, originalUrl, ikigaiOrigin, supersetUrl);
 
     // url.searchParams.delete('edit');
     // window.location.assign(url);
@@ -627,50 +626,6 @@ class Header extends React.PureComponent {
                       >
                         {t('Save')}
                       </Button>
-                      <Dropdown
-                        overlay={
-                          <HeaderActionsDropdown
-                            addSuccessToast={this.props.addSuccessToast}
-                            addDangerToast={this.props.addDangerToast}
-                            dashboardId={dashboardInfo.id}
-                            dashboardTitle={dashboardTitle}
-                            dashboardInfo={dashboardInfo}
-                            dataMask={dataMask}
-                            layout={layout}
-                            expandedSlices={expandedSlices}
-                            customCss={customCss}
-                            colorNamespace={colorNamespace}
-                            colorScheme={colorScheme}
-                            onSave={onSave}
-                            onChange={onChange}
-                            forceRefreshAllCharts={this.forceRefresh}
-                            startPeriodicRender={this.startPeriodicRender}
-                            refreshFrequency={refreshFrequency}
-                            shouldPersistRefreshFrequency={
-                              shouldPersistRefreshFrequency
-                            }
-                            setRefreshFrequency={setRefreshFrequency}
-                            updateCss={updateCss}
-                            editMode={editMode}
-                            hasUnsavedChanges={hasUnsavedChanges}
-                            userCanEdit={userCanEdit}
-                            userCanShare={userCanShare}
-                            userCanSave={userCanSaveAs}
-                            userCanCurate={userCanCurate}
-                            isLoading={isLoading}
-                            showPropertiesModal={this.showPropertiesModal}
-                            manageEmbedded={this.showEmbedModal}
-                            refreshLimit={refreshLimit}
-                            refreshWarning={refreshWarning}
-                            lastModifiedTime={lastModifiedTime}
-                            isDropdownVisible={this.state.isDropdownVisible}
-                            setIsDropdownVisible={this.setIsDropdownVisible}
-                            logEvent={logEvent}
-                          />
-                        }
-                      >
-                        <Icons.MoreVert iconColor="red" />
-                      </Dropdown>
                     </div>
                   )}
                 </div>
@@ -705,7 +660,44 @@ class Header extends React.PureComponent {
             visible: this.state.isDropdownVisible,
             onVisibleChange: this.setIsDropdownVisible,
           }}
-          additionalActionsMenu={<></>}
+          additionalActionsMenu={
+            <ConnectedHeaderActionsDropdown
+              addSuccessToast={this.props.addSuccessToast}
+              addDangerToast={this.props.addDangerToast}
+              dashboardId={dashboardInfo.id}
+              dashboardTitle={dashboardTitle}
+              dashboardInfo={dashboardInfo}
+              dataMask={dataMask}
+              layout={layout}
+              expandedSlices={expandedSlices}
+              customCss={customCss}
+              colorNamespace={colorNamespace}
+              colorScheme={colorScheme}
+              onSave={onSave}
+              onChange={onChange}
+              forceRefreshAllCharts={this.forceRefresh}
+              startPeriodicRender={this.startPeriodicRender}
+              refreshFrequency={refreshFrequency}
+              shouldPersistRefreshFrequency={shouldPersistRefreshFrequency}
+              setRefreshFrequency={setRefreshFrequency}
+              updateCss={updateCss}
+              editMode={editMode}
+              hasUnsavedChanges={hasUnsavedChanges}
+              userCanEdit={userCanEdit}
+              userCanShare={userCanShare}
+              userCanSave={userCanSaveAs}
+              userCanCurate={userCanCurate}
+              isLoading={isLoading}
+              showPropertiesModal={this.showPropertiesModal}
+              manageEmbedded={this.showEmbedModal}
+              refreshLimit={refreshLimit}
+              refreshWarning={refreshWarning}
+              lastModifiedTime={lastModifiedTime}
+              isDropdownVisible={this.state.isDropdownVisible}
+              setIsDropdownVisible={this.setIsDropdownVisible}
+              logEvent={logEvent}
+            />
+          }
           showFaveStar={user?.userId && dashboardInfo?.id}
           showTitlePanelItems
         />
