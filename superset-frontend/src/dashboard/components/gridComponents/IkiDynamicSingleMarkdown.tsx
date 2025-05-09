67b6ed3bbe7bc6348e90a388db43f33eeb9281a1
aridhwan-ikigai <aridhwan@ikigailabs.io>
2025-05-09 10:57:06 -0400

    ipc service

--- superset-frontend/src/dashboard/components/gridComponents/IkiDynamicSingleMarkdown.tsx
+++ superset-frontend/src/dashboard/components/gridComponents/IkiDynamicSingleMarkdown.tsx
@@ -2,12 +2,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { SafeMarkdown } from '@superset-ui/core';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import { refreshChart } from 'src/components/Chart/chartAction';
+import { ContextService } from 'src/service/context-service/context-service';
 import DeleteComponentButton from 'src/dashboard/components/DeleteComponentButton';
 import MarkdownModeDropdown from 'src/dashboard/components/menu/MarkdownModeDropdown';
 import WithPopoverMenu from 'src/dashboard/components/menu/WithPopoverMenu';
@@ -21,15 +21,12 @@ import {
 import { DYNAMIC_MARKDOWN, editorModes, orientations } from '../../constants';
 import {
   EditorMode,
-  IpcMessage,
   LayoutItem,
   LayoutItemWithCustomMarkdown,
   RootState,
 } from '../../types';
 import { COLUMN_TYPE, ROW_TYPE } from '../../util/componentTypes';
 import DragDroppable from '../dnd/DragDroppable';
-import { LOG_ACTIONS_FORCE_REFRESH_CHART } from '../../../logger/LogUtils';
-import { logEvent } from '../../../logger/actions';
 
 const timestamp = new Date().getTime().toString();
 
@@ -61,10 +58,7 @@ type PropTypes = {
   updateComponents: (nextComponents: Record<string, any>) => void;
 };
 
-const paths = window.location.pathname.split('/');
-const dashboardIndex = paths.indexOf('dashboard');
-const supersetDashboardId =
-  dashboardIndex !== -1 ? parseInt(paths[dashboardIndex + 1], 10) : null;
+const { projectId } = ContextService;
 
 const IkiDynamicSingleMarkdown = ({
   id,
@@ -82,12 +76,6 @@ const IkiDynamicSingleMarkdown = ({
   onResizeStart,
   onResizeStop,
 }: PropTypes) => {
-  const dispatch = useDispatch();
-
-  const dashboardLayout = useSelector(
-    (state: RootState) => state.dashboardLayout,
-  );
-
   const ikigaiOrigin = useSelector(
     (state: RootState) => state.dashboardState.ikigaiOrigin,
   );
@@ -97,10 +85,7 @@ const IkiDynamicSingleMarkdown = ({
 
   const {
     meta: {
-      customMarkdown: {
-        custom_markdown_id: customMarkdownId,
-        project_id: projectId,
-      },
+      customMarkdown: { custom_markdown_id: customMarkdownId },
     },
   } = component;
 
@@ -113,68 +98,8 @@ const IkiDynamicSingleMarkdown = ({
     parentComponent.type === ROW_TYPE ? orientations.COLUMN : orientations.ROW;
 
   useEffect(() => {
-    function handleMessage(event: MessageEvent) {
-      if (event.origin !== ikigaiOrigin) return;
-
-      const message = event.data as Partial<IpcMessage>;
-
-      switch (message.type) {
-        case 'notifyUpdateCharts':
-          handleRefreshCharts(message.payload);
-          break;
-
-        case 'getDashboardLayout':
-          handleSendDashboardLayout(message);
-          break;
-
-        default:
-          break;
-      }
-    }
-
-    window.addEventListener('message', handleMessage);
-    return () => window.removeEventListener('message', handleMessage);
+    ContextService.sendDashboardLayout();
   }, []);
-
-  function handleRefreshCharts(selectedChartIds: string[]) {
-    const layoutElements = dashboardLayout.present;
-
-    // for testing chart refresh since we can't import charts from platform
-    // selectedChartIds = [...selectedChartIds, '1179'];
-
-    selectedChartIds.forEach(chartId => {
-      Object.keys(layoutElements).forEach(ele => {
-        const supChartId = layoutElements[ele].meta?.chartId;
-
-        if (supChartId && String(supChartId) === chartId) {
-          logEvent(LOG_ACTIONS_FORCE_REFRESH_CHART, {
-            slice_id: chartId,
-            is_cached: false,
-          });
-
-          dispatch(refreshChart(chartId, true, supersetDashboardId));
-        }
-      });
-    });
-  }
-
-  function handleSendDashboardLayout(message: Partial<IpcMessage>) {
-    if (!dashboardLayout) return;
-
-    const iframes = document.querySelectorAll('iframe');
-
-    const crossWindowMessage: IpcMessage = {
-      type: message.type ?? '',
-      correlationId: message.correlationId ?? '',
-      payload: dashboardLayout.present,
-    };
-
-    iframes.forEach(iframe => {
-      if (!iframe.name.includes(DYNAMIC_MARKDOWN)) return;
-      if (!iframe.contentWindow) return;
-      iframe.contentWindow.postMessage(crossWindowMessage, ikigaiOrigin);
-    });
-  }
 
   function handleChangeEditorMode(newEditorMode: EditorMode) {
     setEditorMode(newEditorMode);
@@ -191,6 +116,8 @@ const IkiDynamicSingleMarkdown = ({
 
   function CustomComponentIframe() {
     const url = new URL('/widget/custom', ikigaiOrigin);
+
+    if (!projectId) throw new Error('project_id query param must be truthy!');
 
     url.searchParams.set('project_id', projectId);
     url.searchParams.set('mode', 'preview');
