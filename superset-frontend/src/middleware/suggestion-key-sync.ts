7d2f6b692ff06c917dd6bc34a9ece7a17fa088d5
Alexander Olsen <aolsen@ikigailabs.io>
2025-09-25 09:31:41 -0400

    calculating fetch candidates inclusive of data masks

--- superset-frontend/src/middleware/suggestion-key-sync.ts
+++ superset-frontend/src/middleware/suggestion-key-sync.ts
@@ -1,11 +1,6 @@
-import { SupersetClient } from '@superset-ui/core';
+import { DataMask } from '@superset-ui/core';
 import { Middleware } from 'redux';
-import {
-  CHART_RENDERING_SUCCEEDED,
-  REMOVE_CHART,
-} from 'src/components/Chart/chartAction';
-import { DASHBOARD_INFO_UPDATED } from 'src/dashboard/actions/dashboardInfo';
-import { REMOVE_SLICE } from 'src/dashboard/actions/dashboardState';
+import { UPDATE_DATA_MASK } from 'src/dataMask/actions';
 import {
   SET_DATASOURCE,
   SET_DATASOURCES,
@@ -34,47 +29,37 @@ export function suggestionKeyDeleted(filterId: string) {
 let suggestionKeys: SuggestionKey[] = [];
 
 const suggestionKeySyncMiddleware: Middleware = api => next => async action => {
-  console.log(action);
   const result = next(action);
+
   const resync = () => {
     const rootState = api.getState() as ReturnType<typeof store.getState>;
+    const dataMasks = Object.values(rootState.dataMask) as DataMask[];
+    const relevantFilters = dataMasks
+      .filter(dm => dm.extraFormData?.filters?.length)
+      .flatMap(dm => dm.extraFormData!.filters!);
 
-    /**
-     * `datasources` *can* become stale. This happens when:
-     *
-     * - User is editing Dashboard (in edit mode)
-     * - Chart component is removed
-     *   - this is the only chart referencing a given datasource
-     *   - datasource will remain in the dashboard datasources
-     *   - suggestions now contain stale data
-     *
-     * This issue is very limited is scope and can be ignored for now.
-     */
-    const datasources = Object.values(rootState.datasources);
-    const fetchCandidates = datasources.map(d => {
+    const fetchCandidates = Object.values(rootState.datasources).flatMap(d => {
       const columnNames = d.columns.map(c => c.column_name);
 
-      const relevantSuggestionKeys = columnNames.filter(columnNames => {
-        return suggestionKeys.some(sk => {
-          return sk.suggestionKey === columnNames;
-        });
-      });
+      // Which suggestion keys exist in this datasource?
+      const relevantKeys = columnNames.filter(name =>
+        suggestionKeys.some(sk => sk.suggestionKey === name),
+      );
 
-      return relevantSuggestionKeys.map(key => {
-        return {
-          datasource: d,
-          key,
-        };
-      });
+      // Which filters apply to this datasource?
+      const filtersForDatasource = relevantFilters.filter(f =>
+        columnNames.includes(typeof f.col === 'string' ? f.col : f.col.label!),
+      );
+
+      return relevantKeys.map(key => ({
+        datasource: d,
+        key,
+        filters: filtersForDatasource,
+      }));
     });
 
-    // const fetchResults = suggestionService.fetchAll(fetchCandidates);
-    // const allColumns = suggestionKeys.map((sk) => sk.suggestionKey);
-    // const uniqueColumns = new Set([allColumns]);
-    // const suggestionsPerColumn = uniqueColumns.map(() => ... iterate thru fetches, flat mapping and making unique all column values (suggestions) ...)
-    // ContextService.sendSuggests(suggestionsPerColumn);
-    // for all current datasources, merge suggestions and pass to frontend
-
+    // const suggestions = SuggestionService.fetchSuggestions(fetchCandidates);
+    // ContextService.sendSuggestions(suggestions);
     console.log(fetchCandidates);
   };
 
@@ -90,46 +75,10 @@ const suggestionKeySyncMiddleware: Middleware = api => next => async action => {
 
   // Covers initial datasources bootstrap, and addition of new datasources via
   // adding a chart to a dashboard.
-  if ([SET_DATASOURCES, SET_DATASOURCE].includes(action.type)) {
+  if (
+    [SET_DATASOURCES, SET_DATASOURCE, UPDATE_DATA_MASK].includes(action.type)
+  ) {
     resync();
-    // const formData = new FormData();
-    // formData.append(
-    //   'form_data',
-    //   JSON.stringify({
-    //     datasource: '2__table',
-    //     viz_type: 'filter_box',
-    //     slice_id: 553,
-    //     granularity_sqla: 'ds',
-    //     time_grain_sqla: 'P1D',
-    //     time_range: 'No filter',
-    //     filter_configs: [
-    //       {
-    //         asc: true,
-    //         clearable: true,
-    //         column: 'name',
-    //         key: 'SJoiVfJhll',
-    //         multiple: true,
-    //         searchAllOptions: false,
-    //       },
-    //     ],
-    //     date_filter: false,
-    //     adhoc_filters: [],
-    //     extra_form_data: {},
-    //     shared_label_colors: {},
-    //     extra_filters: [],
-    //   }),
-    // );
-    // const r = await SupersetClient.post({
-    //   url:
-    //     (slices[0] as any).slice_url.replace('explore', 'explore_json') +
-    //     '&dashboard_id=11',
-    //   // headers: {
-    //   //   'Content-Disposition': 'form-data',
-    //   //   name: 'form_data',
-    //   // },
-    //   body: formData,
-    // });
-    // resync();
   }
 
   return result;
