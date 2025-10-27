a0e8048f46a352665e92fd3ffe4cf13c1e3f7e48
Alexander Olsen <aolsen@ikigailabs.io>
2025-10-27 13:48:56 -0400

    rename datepicker popup file

--- superset-frontend/src/dashboard/components/Dashboard.jsx
+++ superset-frontend/src/dashboard/components/Dashboard.jsx
@@ -42,7 +42,7 @@ import '../stylesheets/index.less';
 import getLocationHash from '../util/getLocationHash';
 import isDashboardEmpty from '../util/isDashboardEmpty';
 import { getAffectedOwnDataCharts } from '../util/charts/getOwnDataCharts';
-import { DatepickerPopup } from 'src/ikigai/datepicker/datepicker-popup';
+import { DatepickerPopup } from 'src/ikigai/datepicker/DatepickerPopup';
 
 const propTypes = {
   actions: PropTypes.shape({
---
+++ superset-frontend/src/ikigai/datepicker/DatepickerPopup.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { DatePicker } from 'antd';
+import { Moment } from 'moment';
+
+import {
+  selectDatepickerVisible,
+  selectDatepickerCoords,
+  hideDatepicker,
+} from './datepicker-slice';
+import { dateSelected } from './datepicker-middleware';
+
+export function DatepickerPopup() {
+  const dispatch = useDispatch();
+  const visible = useSelector(selectDatepickerVisible);
+  const coords = useSelector(selectDatepickerCoords);
+
+  if (!visible || !coords) return null;
+
+  const handleChange = (value: Moment | null) => {
+    dispatch(dateSelected(value ? value.toISOString() : null));
+    dispatch(hideDatepicker());
+  };
+
+  const handleOpenChange = (open: boolean) => {
+    if (!open) dispatch(dateSelected(null));
+  };
+
+  const onBlur = () => {
+    dispatch(hideDatepicker());
+  };
+
+  const popup = (
+    <DatePicker
+      autoFocus
+      open={visible}
+      style={{
+        backgroundColor: 'blue',
+        height: 0,
+        opacity: 0,
+        top: coords.y,
+        left: coords.x,
+        position: 'absolute',
+        zIndex: 1,
+      }}
+      dropdownAlign={{
+        offset: [0, -10],
+      }}
+      popupStyle={{
+        zIndex: 1,
+      }}
+      onBlur={onBlur}
+      dropdownClassName="ikigai-context-datepicker"
+      onChange={handleChange}
+      onOpenChange={handleOpenChange}
+    />
+  );
+
+  return ReactDOM.createPortal(popup, document.body);
+}
