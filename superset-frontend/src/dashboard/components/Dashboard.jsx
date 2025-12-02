ecbbf8b5343ea6156b7aa7648618ceaf78be5029
Kevin Aliberti <kevin@ikigailabs.io>
2025-12-02 14:23:42 -0500

    eslint

--- superset-frontend/src/dashboard/components/Dashboard.jsx
+++ superset-frontend/src/dashboard/components/Dashboard.jsx
@@ -44,8 +44,6 @@ import '../stylesheets/index.less';
 import getLocationHash from '../util/getLocationHash';
 import isDashboardEmpty from '../util/isDashboardEmpty';
 import { getAffectedOwnDataCharts } from '../util/charts/getOwnDataCharts';
-import { DatepickerPopup } from 'src/ikigai/datepicker/DatepickerPopup';
-import { DropdownOptionsPopup } from 'src/ikigai/dropdown-options/DropdownOptionsPopup';
 
 const propTypes = {
   actions: PropTypes.shape({
